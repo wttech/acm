dc668016b05cb8edc9d04ea1166c2327b182ad19
Krystian Panek <krystian.panek@wundermanthompson.com>
2024-11-11 23:22:59 +0100

    Minor

--- core/src/main/java/com/wttech/aem/migrator/core/script/ExecutionQueue.java
+++ core/src/main/java/com/wttech/aem/migrator/core/script/ExecutionQueue.java
@@ -3,10 +3,16 @@ package com.wttech.aem.migrator.core.script;
 import com.wttech.aem.migrator.core.MigratorException;
 import com.wttech.aem.migrator.core.instance.HealthChecker;
 import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import org.apache.sling.event.jobs.Job;
 import org.apache.sling.event.jobs.JobManager;
 import org.apache.sling.event.jobs.consumer.JobConsumer;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,6 +38,8 @@ public class ExecutionQueue implements JobConsumer {
     @Reference
     private Executor executor;
 
+    private ExecutorService executorService;
+
     public Optional<ExecutionJob> add(Executable executable) throws MigratorException {
         var job = jobManager.addJob(TOPIC, Code.toJobProps(executable));
         if (job == null) {
@@ -40,21 +48,57 @@ public class ExecutionQueue implements JobConsumer {
         return Optional.of(new ExecutionJob(job.getId(), job.getJobState().name()));
     }
 
+    @Activate
+    protected void activate() {
+        executorService = Executors.newSingleThreadExecutor();
+    }
+
+    @Deactivate
+    protected void deactivate() {
+        if (executorService != null) {
+            executorService.shutdown();
+        }
+    }
+
     @Override
     public JobResult process(Job job) {
         var executable = Code.fromJob(job);
-        if (healthChecker.isHealthy()) {
+        if (!healthChecker.isHealthy()) {
             LOG.warn("Cancelling execution '{}' - instance is not healthy", executable);
             return JobResult.CANCEL;
         }
-        LOG.info("Executing '{}'", executable);
+        LOG.info("Executing asynchronously '{}'", executable);
+        Future<?> future = executorService.submit(() -> {
+            try {
+                executor.execute(executable);
+            } catch (MigratorException e) {
+                LOG.error("Cannot execute asynchronously '{}'", executable, e);
+            }
+        });
+
+        while (!future.isDone()) {
+            if (job.getJobState() == Job.JobState.STOPPED || Thread.currentThread().isInterrupted()) {
+                future.cancel(true);
+                LOG.info("Job '{}' was cancelled", executable);
+                return JobResult.CANCEL;
+            }
+            try {
+                Thread.sleep(1000); // TODO make this configurable
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                LOG.info("Job '{}' was interrupted", executable);
+                return JobResult.CANCEL;
+            }
+        }
+
         try {
-            executor.execute(executable);
-        } catch (MigratorException e) {
-            LOG.error("Cannot execute '{}'", executable, e);
+            future.get();
+        } catch (Exception e) {
+            LOG.error("Error executing asynchronously '{}'", executable, e);
             return JobResult.FAILED;
         }
-        LOG.info("Executed '{}'", executable);
+
+        LOG.info("Executed asynchronously '{}'", executable);
         return JobResult.OK;
     }
 
