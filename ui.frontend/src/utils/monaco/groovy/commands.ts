25c63ae97e83e45b25cdd58635a691bfe984d781
Krystian Panek <krystian.panek@wundermanthompson.com>
2024-11-24 14:58:52 +0100

    Import almost works

--- ui.frontend/src/utils/monaco/groovy/commands.ts
+++ ui.frontend/src/utils/monaco/groovy/commands.ts
@@ -1,10 +1,28 @@
 import { Monaco } from "@monaco-editor/react";
-import * as monaco from 'monaco-editor';
 
 export function registerCommands(instance: Monaco) {
-    instance.editor.registerCommand('addQualifiedImport', (_, fullyQualifiedClassName) => {
+    instance.editor.registerCommand('importClass', (accessor, ...args) => {
+        const [fullyQualifiedClassName] = args;
         const model = instance.editor.getModels()[0];
 
-        // TODO ...
+        // Add the import statement at the beginning of the file
+        const importStatement = `import ${fullyQualifiedClassName};\n`;
+        const fullText = model.getValue();
+        const newText = importStatement + fullText;
+        model.setValue(newText);
+
+        // Find all instances of the fully qualified class name and replace them with the short class name
+        const shortClassName = fullyQualifiedClassName.split('.').pop();
+        const regex = new RegExp(`\\b${fullyQualifiedClassName}\\b`, 'g');
+        const lines = newText.split('\n');
+        const updatedLines = lines.map(line => {
+            if (line.startsWith('import ')) {
+                return line;
+            }
+            return line.replace(regex, shortClassName);
+        });
+        const updatedText = updatedLines.join('\n');
+
+        model.setValue(updatedText);
     });
 }
