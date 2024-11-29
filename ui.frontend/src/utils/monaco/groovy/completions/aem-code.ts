ff07325a8ca9351949b4855177bcb383cb399f10
Krystian Panek <krystian.panek@wundermanthompson.com>
2024-11-29 08:53:19 +0100

    Word completion

--- ui.frontend/src/utils/monaco/groovy/completions/aem-code.ts
+++ ui.frontend/src/utils/monaco/groovy/completions/aem-code.ts
@@ -5,11 +5,10 @@ import {ApiDataAssistCode, apiRequest} from "../../../api.ts";
 
 export function registerAemCodeCompletions(instance: Monaco) {
     instance.languages.registerCompletionItemProvider(LANGUAGE_ID, {
+
         provideCompletionItems: async (model: monaco.editor.ITextModel, position: monaco.Position): Promise<monaco.languages.CompletionList> => {
             const wordAtPosition = model.getWordAtPosition(position);
             const wordText = wordAtPosition?.word || '';
-
-            console.info('Code assistance word:', wordText);
 
             try {
                 const response = await apiRequest<ApiDataAssistCode>({
@@ -18,10 +17,7 @@ export function registerAemCodeCompletions(instance: Monaco) {
                     operation: "Code assistance"
                 });
                 const assistance = response.data.data;
-
-                // TODO support completing when cursor is in the middle of a word
-
-                const filteredSuggestions = (assistance?.suggestions ?? []).map(suggestion => ({
+                const suggestions = (assistance?.suggestions ?? []).map(suggestion => ({
                     label: suggestion.v,
                     insertText: suggestion.v,
                     kind: mapToMonacoKind(suggestion.k),
@@ -33,10 +29,11 @@ export function registerAemCodeCompletions(instance: Monaco) {
                         position.lineNumber,
                         wordAtPosition?.endColumn || position.column
                     ),
-                    // TODO: sortText: sortText(suggestion.v, wordText)
+                    // TODO below does not work, Monaco bug? (we want to prioritize exact class name matches)
+                    // sortText: sortText(suggestion.v, wordText)
                 }));
 
-                return { suggestions: filteredSuggestions, incomplete: true };
+                return { suggestions: suggestions, incomplete: true };
             } catch (error) {
                 console.error('Code assistance error:', error);
                 return { suggestions: [], incomplete: true };
@@ -59,3 +56,13 @@ function mapToMonacoKind(kind: string): monaco.languages.CompletionItemKind {
             return monaco.languages.CompletionItemKind.Text;
     }
 }
+
+/*
+function sortText(label: string, word: string): string {
+    const lastSegment = label.split('.').pop() || '';
+    const isExactMatch = lastSegment === word;
+    const isPartialMatch = lastSegment.includes(word);
+    const score = (isExactMatch ? '0' : isPartialMatch ? '1' : '2');
+    return score + "_" + label;
+}
+*/
