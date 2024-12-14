c976832e4f82d2bbf15679eff4e12a4ba8e17c25
Krystian Panek <krystian.panek@wundermanthompson.com>
2024-12-14 13:12:37 +0100

    Layout works nicely

--- ui.frontend/src/App.tsx
+++ ui.frontend/src/App.tsx
@@ -18,24 +18,16 @@ function App() {
         <Provider theme={defaultTheme} colorScheme={"light"}>
             <HashRouter>
                 <Flex direction="column" height="100vh">
-                    <View 
-                        flex
-                        UNSAFE_style={{ boxSizing: 'border-box' }}
-                        padding="size-200"
-                    >
-                        <Flex
-                            direction="column"
-                            height="100%"
-                            gap="size-100"
-                        >
-                            <View marginBottom="size-100">
-                                <Header/>
-                            </View>
-                            <View flex>
-                                <Content/>
-                            </View>
-                            <Footer/>
-                        </Flex>
+                    <View paddingX="size-200" paddingTop="size-200">
+                        <View marginBottom="size-100">
+                            <Header/>
+                        </View>
+                    </View>
+                    <View flex paddingX="size-200">
+                        <Content/>
+                    </View>
+                    <View paddingX="size-200" paddingBottom="size-200">
+                        <Footer/>
                     </View>
                 </Flex>
             </HashRouter>
