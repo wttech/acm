6f34ef3bfaff30b178109d81c70d4f2aaf365d07
Krystian Panek <krystian.panek@vml.com>
2025-02-10 10:10:40 +0100

    Snippets clean up

--- ui.frontend/src/pages/ConsoleCode.groovy.ts
+++ ui.frontend/src/pages/ConsoleCode.groovy.ts
@@ -1,39 +1,9 @@
 export default `
 boolean canRun() {
-    return condition.always()
+  return condition.always()
 }
-
+  
 void doRun() {
-    println "Processing..."
-
-    println "Updating ACLs..."
-    def acmeService = acl.createUser { id = "acme.service"; systemUser(); skipIfExists() }
-    acl.getUser { authorizable = acmeService }?.with {
-        purge { }
-        allow { path = "/content"; permissions = ["jcr:read", "jcr:write"] }
-    }
-
-    def johnDoe = acl.createUser { id = "john.doe"; fullName = "John Doe"; password = "ilovekittens"; skipIfExists() }
-    johnDoe?.with {
-        purge()
-        allow("/content", ["jcr:read"])
-    }
-    johnDoe.purge()
-    johnDoe.purge()
-
-    acl.createGroup { id = "test.group" }.with {
-        removeAllMembers()
-        addMember(acmeService)
-        addMember(johnDoe)
-    }
-
-    println "Updating JCR resources..."
-    def max = 10
-    for (int i = 0; i < max; i++) {
-        Thread.sleep(500)
-        println "Updated (\${i + 1}/\${max})"
-    }
-
-    println "Processing done"
+  println "Hello World!"
 }
 `.trim();
