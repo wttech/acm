98056031c30ef86ed6c4f1e1857489e46968f203
Dominik Przybyl <dominik.przybyl@vml.com>
2025-02-17 10:22:31 +0100

    refactor

--- core/src/main/java/com/wttech/aem/contentor/core/utils/Utils.java
+++ core/src/main/java/com/wttech/aem/contentor/core/utils/Utils.java
@@ -1,10 +1,13 @@
 package com.wttech.aem.contentor.core.utils;
 
+import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.PatternLayout;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.OutputStreamAppender;
 import java.io.OutputStream;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.slf4j.LoggerFactory;
 
 public class Utils {
@@ -15,28 +18,31 @@ public class Utils {
         this.out = (OutputStreamWrapper) out;
     }
 
-    public void duplicateLogToOutput(String loggerName) {
+    public List<String> duplicateLogToOutput(String loggerName) {
         LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
-        context.getLoggerList().stream()
+        List<Logger> loggers = context.getLoggerList().stream()
                 .filter(logger -> logger.getName().contains(loggerName))
-                .forEach(logger -> {
-                    out.registerLogger(logger);
+                .filter(logger -> logger.getAppender(out.getAppenderName()) == null)
+                .collect(Collectors.toList());
+        loggers.forEach(logger -> {
+            out.registerLogger(logger);
 
-                    OutputStreamAppender<ILoggingEvent> appender = new OutputStreamAppender<>();
-                    appender.setName(out.getAppenderName());
-                    appender.setContext(context);
-                    appender.setOutputStream(out);
+            OutputStreamAppender<ILoggingEvent> appender = new OutputStreamAppender<>();
+            appender.setName(out.getAppenderName());
+            appender.setContext(context);
+            appender.setOutputStream(out);
 
-                    PatternLayout layout = new PatternLayout();
-                    layout.setContext(context);
-                    layout.setPattern("%msg%n");
-                    layout.start();
+            PatternLayout layout = new PatternLayout();
+            layout.setContext(context);
+            layout.setPattern("%msg%n");
+            layout.start();
 
-                    appender.setLayout(layout);
-                    appender.start();
+            appender.setLayout(layout);
+            appender.start();
 
-                    logger.addAppender(appender);
-                    logger.setAdditive(false);
-                });
+            logger.addAppender(appender);
+            logger.setAdditive(false);
+        });
+        return loggers.stream().map(Logger::getName).collect(Collectors.toList());
     }
 }
