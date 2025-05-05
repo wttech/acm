3197204b599780d22c6503d0ed4c796968f47b7a
Krystian Panek <krystian.panek@vml.com>
2025-05-05 09:47:11 +0200

    Hotfix

--- core/src/main/java/com/vml/es/aem/acm/core/util/YamlUtils.java
+++ core/src/main/java/com/vml/es/aem/acm/core/util/YamlUtils.java
@@ -1,38 +1,24 @@
 package com.vml.es.aem.acm.core.util;
 
-import java.io.BufferedInputStream;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import org.yaml.snakeyaml.DumperOptions;
-import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.constructor.Constructor;
-import org.yaml.snakeyaml.introspector.BeanAccess;
-import org.yaml.snakeyaml.representer.Representer;
 
 public final class YamlUtils {
 
-    private static final Yaml YAML;
+    private static final ObjectMapper YAML_MAPPER = new ObjectMapper(new YAMLFactory());
 
-    static {
-        YAML = customYaml();
+    private YamlUtils() {
+        // intentionally empty
     }
 
-    private static Yaml customYaml() {
-        Constructor constructor = new Constructor();
-        constructor.getPropertyUtils().setSkipMissingProperties(true);
-        DumperOptions options = new DumperOptions();
-        Representer representer = new Representer();
-        Yaml yaml = new Yaml(constructor, representer, options);
-        yaml.setBeanAccess(BeanAccess.FIELD);
-        return yaml;
+    public static <T> T readYaml(InputStream inputStream, Class<T> clazz) throws IOException {
+        return YAML_MAPPER.readValue(inputStream, clazz);
     }
 
-    public static <T> T readYaml(InputStream inputStream, Class<T> clazz) {
-        return YAML.loadAs(new BufferedInputStream(inputStream), clazz);
-    }
-
-    public static void writeYaml(OutputStream outputStream, Object data) {
-        YAML.dump(data, new OutputStreamWriter(outputStream));
+    public static void writeYaml(OutputStream outputStream, Object data) throws IOException {
+        YAML_MAPPER.writeValue(outputStream, data);
     }
 }
