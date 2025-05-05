21d8707e0f44c1e2ba066d7c51bdb84d6136e4b9
Krystian Panek <krystian.panek@vml.com>
2025-05-05 16:31:06 +0200

    TODO

--- ui.frontend/src/components/ExecutionProgressBar.tsx
+++ ui.frontend/src/components/ExecutionProgressBar.tsx
@@ -1,5 +1,6 @@
 import { Meter, ProgressBar } from '@adobe/react-spectrum';
-import React, {useEffect, useState} from 'react';
+import React, { useState } from 'react';
+import { useInterval } from 'react-use';
 import { useFormatter } from '../hooks/formatter';
 import { useScriptStats } from '../hooks/script';
 import { Execution, ExecutionStatus, isExecutionActive, isExecutionCompleted, isExecutionPending } from '../utils/api.types';
@@ -11,29 +12,34 @@ interface ExecutionProgressBarProps {
 }
 
 const ExecutionProgressBar: React.FC<ExecutionProgressBarProps> = ({ execution, active }) => {
+  // TODO now it's laggy; only pending execution should ask for stats, etc
+
   const formatter = useFormatter();
   const stats = useScriptStats(execution?.executable.id || null);
   const [progress, setProgress] = useState<number | null>(null);
   const [label, setLabel] = useState<string>('Not executing');
 
-  useEffect(() => {
-    if (execution && stats) {
-      if (execution.startDate && stats.averageDuration) {
-        const elapsedTime = formatter.durationTillNow(execution.startDate)!;
-        const percentage = Math.min((elapsedTime / stats.averageDuration) * 100, 100);
+  useInterval(
+    () => {
+      if (execution && stats) {
+        if (execution.startDate && stats.averageDuration) {
+          const elapsedTime = formatter.durationTillNow(execution.startDate)!;
+          const percentage = Math.min((elapsedTime / stats.averageDuration) * 100, 100);
 
-        setProgress(percentage);
-        if (isExecutionPending(execution.status) || isExecutionActive(execution.status)) {
-          setLabel(percentage >= 100 ? 'Running - Almost done' : `Running - ${formatter.durationShort(stats.averageDuration - elapsedTime)} remaining`);
-        } else if (isExecutionCompleted(execution.status)) {
-          setLabel(Strings.capitalize(execution.status));
+          setProgress(percentage);
+          if (isExecutionPending(execution.status) || isExecutionActive(execution.status)) {
+            setLabel(percentage >= 100 ? 'Running - Almost done' : `Running - ${formatter.durationShort(stats.averageDuration - elapsedTime)} remaining`);
+          } else if (isExecutionCompleted(execution.status)) {
+            setLabel(`${Strings.capitalize(execution.status)} after ${formatter.durationShort(execution.duration)}`);
+          }
+        } else {
+          setProgress(null);
+          setLabel(isExecutionCompleted(execution.status) ? Strings.capitalize(execution.status) : 'Running - Stay tuned');
         }
-      } else {
-        setProgress(null);
-        setLabel(isExecutionCompleted(execution.status) ? Strings.capitalize(execution.status) : 'Running - Stay tuned');
       }
-    }
-  }, [execution?.id, execution?.startDate, execution?.status, stats?.averageDuration]);
+    },
+    execution && stats ? 1000 : null,
+  );
 
   const variant = ((): 'positive' | 'informative' | 'warning' | 'critical' | undefined => {
     switch (execution?.status) {
@@ -54,12 +60,13 @@ const ExecutionProgressBar: React.FC<ExecutionProgressBarProps> = ({ execution, 
   }
 
   if (active || isExecutionPending(execution.status)) {
-    if (progress !== null) {
+    if (progress !== null && progress < 100) {
       return <ProgressBar aria-label={label} label={label} value={progress} showValueLabel={false} />;
     }
     return <ProgressBar aria-label={label} showValueLabel={false} label={label} isIndeterminate />;
   }
-  return <Meter aria-label={label} variant={variant} showValueLabel={false} value={100} label={`${label} after ${formatter.durationShort(execution.duration)}`} />;
+
+  return <Meter aria-label={label} variant={variant} showValueLabel={false} value={100} label={label} />;
 };
 
 export default ExecutionProgressBar;
