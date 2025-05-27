d3cbf745d64ae0e39d22d451660b31d7035ae3de
Krystian Panek <krystian.panek@vml.com>
2025-05-27 09:19:59 +0200

    Doc links work perfect

--- core/src/main/java/dev/vml/es/acm/core/code/CodeRepository.java
+++ core/src/main/java/dev/vml/es/acm/core/code/CodeRepository.java
@@ -1,7 +1,6 @@
 package dev.vml.es.acm.core.code;
 
 import dev.vml.es.acm.core.assist.JavaClassDictionary;
-import dev.vml.es.acm.core.format.TemplateFormatter;
 import dev.vml.es.acm.core.osgi.ClassInfo;
 import java.util.Collections;
 import java.util.HashMap;
@@ -28,21 +27,26 @@ public class CodeRepository {
                                 + " Used to generate links to class documentation in code assistance."
                                 + " Available variables: ${pkg} - package name, ${clazz} - class name.")
         String[] classLinkMappings() default {
-            "dev.vml.es.acm.core=https://github.com/wttech/acm/blob/main/core/src/main/java/${pkg}/${clazz}.java",
-            "org.apache.sling.api=https://github.com/apache/sling-org-apache-sling-api/tree/master/src/main/java/${pkg}/${clazz}.java",
+            "dev.vml.es.acm.core=https://github.com/wttech/acm/blob/main/core/src/main/java/%s.java",
+            "org.apache.commons.lang3=https://commons.apache.org/proper/commons-lang/apidocs/%s.html",
+            "org.apache.sling.api=https://github.com/apache/sling-org-apache-sling-api/tree/master/src/main/java/%s.java",
+            "org.apache.sling.models=https://github.com/apache/sling-org-apache-sling-models-api/tree/master/src/main/java/%s.java",
+            "com.day.cq=https://developer.adobe.com/experience-manager/reference-materials/6-5/javadoc/%s.html",
+            "com.adobe.cq=https://developer.adobe.com/experience-manager/reference-materials/6-5/javadoc/%s.html",
+            "com.adobe.granite=https://developer.adobe.com/experience-manager/reference-materials/6-5/javadoc/%s.html",
         };
 
         @AttributeDefinition(
                 name = "Class Link for RT Jar",
                 description = "URL to the Java 8 documentation, used for linking classes from runtime jar."
                         + " Available variables: ${clazz} - class name.")
-        String classLinkRtJar() default "https://docs.oracle.com/javase/8/docs/api/${clazz}.html";
+        String classLinkRtJar() default "https://docs.oracle.com/javase/8/docs/api/%s.html";
 
         @AttributeDefinition(
                 name = "Class Link for JMS",
                 description = "URL to the Java 9+ documentation, used for linking classes from JMS."
                         + " Available variables: " + "${module} - module name, ${clazz} - class name.")
-        String classLinkJms() default "https://docs.oracle.com/en/java/javase/${module}/docs/api/${clazz}.html";
+        String classLinkJms() default "https://docs.oracle.com/en/java/javase/%s/docs/api/%s.html";
     }
 
     private Config config;
@@ -68,34 +72,23 @@ public class CodeRepository {
     }
 
     public Optional<String> linkToClass(ClassInfo classInfo) {
-        TemplateFormatter templateFormatter = new TemplateFormatter();
         if (classInfo.getModule() == null) {
             return linkToClass(classInfo.getClassName());
         } else if (JavaClassDictionary.RTJAR_MODULE.equals(classInfo.getModule())) {
-            Map<String, String> params =
-                    Collections.singletonMap("clazz", StringUtils.replace(classInfo.getClassName(), ".", "/"));
-            return Optional.of(templateFormatter.renderString(config.classLinkRtJar(), params));
+            return Optional.of(
+                    String.format(config.classLinkRtJar(), StringUtils.replace(classInfo.getClassName(), ".", "/")));
         } else {
-            Map<String, String> params = new HashMap<>();
-            params.put("module", classInfo.getModule());
-            params.put("clazz", StringUtils.replace(classInfo.getClassName(), ".", "/"));
-            return Optional.of(templateFormatter.renderString(config.classLinkJms(), params));
+            return Optional.of(String.format(
+                    config.classLinkJms(),
+                    classInfo.getModule(),
+                    StringUtils.replace(classInfo.getClassName(), ".", "/")));
         }
     }
 
     public Optional<String> linkToClass(String className) {
-        TemplateFormatter templateFormatter = new TemplateFormatter();
         return classLinks.entrySet().stream()
                 .filter(entry -> StringUtils.startsWith(className, entry.getKey()))
                 .findFirst()
-                .map(entry -> templateFormatter.renderString(
-                        entry.getValue(), mappingLinkParams(entry.getValue(), className)));
-    }
-
-    private Map<String, String> mappingLinkParams(String pkg, String className) {
-        Map<String, String> params = new HashMap<>();
-        params.put("pkg", pkg);
-        params.put("clazz", className);
-        return params;
+                .map(e -> String.format(e.getValue(), StringUtils.replace(className, ".", "/")));
     }
 }
