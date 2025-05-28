28aaf9562006b866e06ed17471f021ba0f7449c4
Krystian Panek <krystian.panek@vml.com>
2025-05-28 08:25:06 +0200

    Arg example

--- ui.content.example/src/main/content/jcr_root/conf/acm/settings/script/manual/example/ACME-201_arguments.groovy
+++ ui.content.example/src/main/content/jcr_root/conf/acm/settings/script/manual/example/ACME-201_arguments.groovy
@@ -39,6 +39,7 @@ void doRun() {
     println "Last Vet Visit: ${args.value('lastVetVisit')}"
     println "Favorite Color: ${args.value('favoriteColor')}"
     println "Secret Code: ${args.value('secretCode')}"
+    println "Profile Picture: ${args.value('profilePicture')}"
 
     if (args.value('allergicToDogs') && args.value('animalType') == 'dog') {
         println "${args.value('animalName')} cannot be around dogs due to allergies!"
