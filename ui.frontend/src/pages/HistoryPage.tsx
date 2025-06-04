fcc288c6682aa00d57b18a91f7936d529ec7692a
Krystian Panek <krystian.panek@vml.com>
2025-06-04 12:24:08 +0200

    Fmt

--- ui.frontend/src/pages/HistoryPage.tsx
+++ ui.frontend/src/pages/HistoryPage.tsx
@@ -9,6 +9,7 @@ import Pause from '@spectrum-icons/workflow/Pause';
 import Search from '@spectrum-icons/workflow/Search';
 import Star from '@spectrum-icons/workflow/Star';
 import User from '@spectrum-icons/workflow/User';
+import equal from 'fast-deep-equal';
 import { useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { useDebounce } from 'react-use';
@@ -21,7 +22,6 @@ import { toastRequest } from '../utils/api';
 import { ExecutionFormat, ExecutionOutput, ExecutionQueryParams, ExecutionStatus, ExecutionSummary, isExecutableExplicit } from '../utils/api.types';
 import { Dates } from '../utils/dates';
 import { Urls } from '../utils/url';
-import equal from 'fast-deep-equal';
 
 const HistoryFilterDelay = 1500;
 
@@ -49,24 +49,26 @@ const HistoryPage = () => {
   const formatter = useFormatter();
 
   useDebounce(
-      () => {
-        const fetchExecutions = async () => {
-          setLoading(true);
-          try {
-            const params = new URLSearchParams();
-            params.append(ExecutionQueryParams.FORMAT, ExecutionFormat.SUMMARY);
-            if (executableId) params.append(ExecutionQueryParams.EXECUTABLE_ID, executableId);
-            if (userId) params.append(ExecutionQueryParams.USER_ID, userId);
-            if (startDate) params.append(ExecutionQueryParams.START_DATE, startDate.toString());
-            if (endDate) params.append(ExecutionQueryParams.END_DATE, endDate.toString());
-            if (status && status !== 'all') params.append(ExecutionQueryParams.STATUS, status);
-            if (durationMin || durationMax) params.append(ExecutionQueryParams.DURATION, `${durationMin || ''},${durationMax || ''}`);
+    () => {
+      const fetchExecutions = async () => {
+        setLoading(true);
+        try {
+          const params = new URLSearchParams();
+          params.append(ExecutionQueryParams.FORMAT, ExecutionFormat.SUMMARY);
+          if (executableId) params.append(ExecutionQueryParams.EXECUTABLE_ID, executableId);
+          if (userId) params.append(ExecutionQueryParams.USER_ID, userId);
+          if (startDate) params.append(ExecutionQueryParams.START_DATE, startDate.toString());
+          if (endDate) params.append(ExecutionQueryParams.END_DATE, endDate.toString());
+          if (status && status !== 'all') params.append(ExecutionQueryParams.STATUS, status);
+          if (durationMin || durationMax) params.append(ExecutionQueryParams.DURATION, `${durationMin || ''},${durationMax || ''}`);
 
-            // TODO setSearchState(params.toString(), {replace: true});
+          setSearchState(params.toString(), { replace: true });
 
-            const response = await toastRequest<ExecutionOutput<ExecutionSummary>>({
-              method: 'GET',
-              url: Urls.compose('/apps/acm/api/execution.json',  (() => {
+          const response = await toastRequest<ExecutionOutput<ExecutionSummary>>({
+            method: 'GET',
+            url: Urls.compose(
+              '/apps/acm/api/execution.json',
+              (() => {
                 const result = new URLSearchParams(params);
                 const wildcard = '@W@';
                 result.delete(ExecutionQueryParams.EXECUTABLE_ID);
@@ -74,106 +76,107 @@ const HistoryPage = () => {
                 result.delete(ExecutionQueryParams.USER_ID);
                 result.append(ExecutionQueryParams.USER_ID, `${wildcard}${userId}${wildcard}`);
                 return result;
-              })()),
-              operation: `Executions loading`,
-              positive: false,
-            });
-            setExecutions(prev => {
-              if (!equal(prev, response.data.data)) {
-                return response.data.data;
-              }
-              return prev
-            });
-          } catch (error) {
-            console.error('Cannot load executions:', error);
-          } finally {
-            setLoading(false);
-          }
-        };
-        fetchExecutions();
-      },
-      HistoryFilterDelay,
-      [startDate, endDate, status, executableId, userId, durationMin, durationMax],
+              })(),
+            ),
+            operation: `Executions loading`,
+            positive: false,
+          });
+          setExecutions((prev) => {
+            if (!equal(prev, response.data.data)) {
+              return response.data.data;
+            }
+            return prev;
+          });
+        } catch (error) {
+          console.error('Cannot load executions:', error);
+        } finally {
+          setLoading(false);
+        }
+      };
+      fetchExecutions();
+    },
+    HistoryFilterDelay,
+    [startDate, endDate, status, executableId, userId, durationMin, durationMax],
   );
 
   const renderEmptyState = () => (
-      <IllustratedMessage>
-        <NotFound />
-        <Content>No executions found</Content>
-      </IllustratedMessage>
+    <IllustratedMessage>
+      <NotFound />
+      <Content>No executions found</Content>
+    </IllustratedMessage>
   );
 
   return (
-      <Flex direction="column" flex="1" gap="size-200" marginY="size-100">
-        <View borderBottomWidth="thick" borderColor="gray-300" paddingBottom="size-200" marginBottom="size-10">
-          <Flex direction="row" gap="size-200" alignItems="center">
-            <TextField flex="1" icon={<Search />} label="Executable" value={executableId} type="search" onChange={setExecutableId} placeholder="e.g. script name" />
-            <TextField flex="1" icon={<User />} label="User" value={userId} type="search" onChange={setUserId} placeholder="ID" />
-            <DatePicker width="size-3000" label="Start Date" granularity="second" value={startDate} onChange={setStartDate} />
-            <DatePicker width="size-3000" label="End Date" granularity="second" value={endDate} onChange={setEndDate} />
-            <NumberField width="size-1600" label="Min Duration (ms)" value={durationMin} onChange={setDurationMin} />
-            <NumberField width="size-1600" label="Max Duration (ms)" value={durationMax} onChange={setDurationMax} />
-            <Picker width="size-2000" label="Status" selectedKey={status} onSelectionChange={(key) => setStatus(String(key))}>
-              <Item textValue="All" key="all">
-                <Star size="S" />
-                <Text>All</Text>
-              </Item>
-              <Item textValue="Skipped" key={ExecutionStatus.SKIPPED}>
-                <Pause size="S" />
-                <Text>Skipped</Text>
-              </Item>
-              <Item textValue="Aborted" key={ExecutionStatus.ABORTED}>
-                <Cancel size="S" />
-                <Text>Aborted</Text>
-              </Item>
-              <Item textValue="Failed" key={ExecutionStatus.FAILED}>
-                <Alert size="S" />
-                <Text>Failed</Text>
-              </Item>
-              <Item textValue="Succeeded" key={ExecutionStatus.SUCCEEDED}>
-                <Checkmark size="S" />
-                <Text>Succeeded</Text>
-              </Item>
-            </Picker>
-          </Flex>
-        </View>
-        {executions === null || loading ? (
-            <Flex flex="1" justifyContent="center" alignItems="center" height="100vh">
-              <ProgressBar label="Loading..." isIndeterminate />
-            </Flex>
-        ) : (
-            <TableView flex="1" aria-label="Executions table" selectionMode="none" renderEmptyState={renderEmptyState} onAction={(key: Key) => navigate(`/executions/view/${encodeURIComponent(key)}`)}>
-              <TableHeader>
-                <Column width="6fr">Executable</Column>
-                <Column width="3fr">User</Column>
-                <Column width="4fr">Started</Column>
-                <Column width="3fr">Duration</Column>
-                <Column width="2fr" align="end">
-                  Status
-                </Column>
-              </TableHeader>
-              <TableBody>
-                {(executions?.list || []).map((execution) => (
-                    <Row key={execution.id}>
-                      <Cell>
-                        <ExecutableIdValue id={execution.executableId} />
-                      </Cell>
-                      <Cell>
-                        <UserInfo id={execution.userId} />
-                      </Cell>
-                      <Cell>
-                        <DateExplained value={execution.startDate} />
-                      </Cell>
-                      <Cell>{formatter.duration(execution.duration)}</Cell>
-                      <Cell>
-                        <ExecutionStatusBadge value={execution.status} />
-                      </Cell>
-                    </Row>
-                ))}
-              </TableBody>
-            </TableView>
-        )}
-      </Flex>
+    <Flex direction="column" flex="1" gap="size-200" marginY="size-100">
+      <View borderBottomWidth="thick" borderColor="gray-300" paddingBottom="size-200" marginBottom="size-10">
+        <Flex direction="row" gap="size-200" alignItems="center">
+          <TextField flex="1" icon={<Search />} label="Executable" value={executableId} type="search" onChange={setExecutableId} placeholder="e.g. script name" />
+          <TextField flex="1" icon={<User />} label="User" value={userId} type="search" onChange={setUserId} placeholder="ID" />
+          <DatePicker width="size-3000" label="Start Date" granularity="second" value={startDate} onChange={setStartDate} />
+          <DatePicker width="size-3000" label="End Date" granularity="second" value={endDate} onChange={setEndDate} />
+          <NumberField width="size-1600" label="Min Duration (ms)" value={durationMin} onChange={setDurationMin} />
+          <NumberField width="size-1600" label="Max Duration (ms)" value={durationMax} onChange={setDurationMax} />
+          <Picker width="size-2000" label="Status" selectedKey={status} onSelectionChange={(key) => setStatus(String(key))}>
+            <Item textValue="All" key="all">
+              <Star size="S" />
+              <Text>All</Text>
+            </Item>
+            <Item textValue="Skipped" key={ExecutionStatus.SKIPPED}>
+              <Pause size="S" />
+              <Text>Skipped</Text>
+            </Item>
+            <Item textValue="Aborted" key={ExecutionStatus.ABORTED}>
+              <Cancel size="S" />
+              <Text>Aborted</Text>
+            </Item>
+            <Item textValue="Failed" key={ExecutionStatus.FAILED}>
+              <Alert size="S" />
+              <Text>Failed</Text>
+            </Item>
+            <Item textValue="Succeeded" key={ExecutionStatus.SUCCEEDED}>
+              <Checkmark size="S" />
+              <Text>Succeeded</Text>
+            </Item>
+          </Picker>
+        </Flex>
+      </View>
+      {executions === null || loading ? (
+        <Flex flex="1" justifyContent="center" alignItems="center" height="100vh">
+          <ProgressBar label="Loading..." isIndeterminate />
+        </Flex>
+      ) : (
+        <TableView flex="1" aria-label="Executions table" selectionMode="none" renderEmptyState={renderEmptyState} onAction={(key: Key) => navigate(`/executions/view/${encodeURIComponent(key)}`)}>
+          <TableHeader>
+            <Column width="6fr">Executable</Column>
+            <Column width="3fr">User</Column>
+            <Column width="4fr">Started</Column>
+            <Column width="3fr">Duration</Column>
+            <Column width="2fr" align="end">
+              Status
+            </Column>
+          </TableHeader>
+          <TableBody>
+            {(executions?.list || []).map((execution) => (
+              <Row key={execution.id}>
+                <Cell>
+                  <ExecutableIdValue id={execution.executableId} />
+                </Cell>
+                <Cell>
+                  <UserInfo id={execution.userId} />
+                </Cell>
+                <Cell>
+                  <DateExplained value={execution.startDate} />
+                </Cell>
+                <Cell>{formatter.duration(execution.duration)}</Cell>
+                <Cell>
+                  <ExecutionStatusBadge value={execution.status} />
+                </Cell>
+              </Row>
+            ))}
+          </TableBody>
+        </TableView>
+      )}
+    </Flex>
   );
 };
 
