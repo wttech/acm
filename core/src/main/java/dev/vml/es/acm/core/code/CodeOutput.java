6bc2813eee494d1d8255e9a4b1e77860a361545d
Krystian Panek <krystian.panek@vml.com>
2025-06-20 09:35:57 +0200

    Closing code print stream

--- core/src/main/java/dev/vml/es/acm/core/code/CodeOutput.java
+++ core/src/main/java/dev/vml/es/acm/core/code/CodeOutput.java
@@ -4,7 +4,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Optional;
 
-public interface Output {
+public interface CodeOutput {
 
     Optional<String> readString();
 
