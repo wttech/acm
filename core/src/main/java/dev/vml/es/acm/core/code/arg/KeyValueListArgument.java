da6e67a4d1f84b45d444adb7de05a27010f17d2f
Krystian Panek <krystian.panek@vml.com>
2025-07-15 10:19:51 +0200

    Map/KV arg impl

--- core/src/main/java/dev/vml/es/acm/core/code/arg/KeyValueListArgument.java
+++ core/src/main/java/dev/vml/es/acm/core/code/arg/KeyValueListArgument.java
@@ -3,8 +3,10 @@ package dev.vml.es.acm.core.code.arg;
 import dev.vml.es.acm.core.code.Argument;
 import dev.vml.es.acm.core.code.ArgumentType;
 import dev.vml.es.acm.core.util.KeyValue;
+
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class KeyValueListArgument extends Argument<List<KeyValue<String, String>>> {
 
@@ -30,5 +32,16 @@ public class KeyValueListArgument extends Argument<List<KeyValue<String, String>
 
     public void setMax(Integer max) {
         this.max = max;
+    }
+
+    public void setValue(List<List<String>> value) {
+        if (value == null) {
+            super.setValue(null);
+        } else {
+            if (value.stream().anyMatch(v -> v.size() != 2)) {
+                throw new IllegalArgumentException("Key-value list must contain pairs of key and value!");
+            }
+            super.setValue(value.stream().map(v -> new KeyValue<>(v.get(0), v.get(1))).collect(Collectors.toList()));
+        }
     }
 }
--- core/src/main/java/dev/vml/es/acm/core/code/arg/MapArgument.java
+++ core/src/main/java/dev/vml/es/acm/core/code/arg/MapArgument.java
@@ -2,6 +2,7 @@ package dev.vml.es.acm.core.code.arg;
 
 import dev.vml.es.acm.core.code.Argument;
 import dev.vml.es.acm.core.code.ArgumentType;
+
 import java.util.Map;
 
 public class MapArgument extends Argument<Map<String, String>> {
