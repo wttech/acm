dce8880ff9b1eb09874d617a6697a903901ea914
Krystian Panek <krystian.panek@vml.com>
2025-07-21 09:39:23 +0200

    Every X methods simplified

--- ui.content.example/src/main/content/jcr_root/conf/acm/settings/script/auto/enabled/example/ACME-25_instance_changed.groovy
+++
@@ -1,7 +0,0 @@
-boolean canRun() {
-    return condition.idleSelf() && (condition.contentChanged() || condition.retryIfInstanceChanged())
-}
-
-void doRun() {
-    println("I should run when content changes or when the instance changes after a failure!")
-}
