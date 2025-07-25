c4bc81defe1faa205f4bb3030742aff9aaeb1a9b
Krystian Panek <krystian.panek@vml.com>
2025-07-25 14:56:13 +0200

    Utility for human-readable name for executables

--- core/src/main/java/dev/vml/es/acm/core/code/ExecutableUtils.java
+++ core/src/main/java/dev/vml/es/acm/core/code/ExecutableUtils.java
@@ -2,6 +2,7 @@ package dev.vml.es.acm.core.code;
 
 import dev.vml.es.acm.core.AcmConstants;
 import dev.vml.es.acm.core.script.ScriptRepository;
+import dev.vml.es.acm.core.script.ScriptType;
 import org.apache.commons.lang3.StringUtils;
 
 public final class ExecutableUtils {
@@ -14,7 +15,13 @@ public final class ExecutableUtils {
         if (Executable.ID_CONSOLE.equals(id)) {
             return "Console";
         }
-        if (StringUtils.startsWith(id, ScriptRepository.ROOT)) {
+        if (StringUtils.startsWith(id, ScriptType.ENABLED.root() + "/")) {
+            return String.format("Automatic Script '%s'", StringUtils.removeStart(id, ScriptType.ENABLED.root() + "/"));
+        }
+        if (StringUtils.startsWith(id, ScriptType.MANUAL.root() + "/")) {
+            return String.format("Manual Script '%s'", StringUtils.removeStart(id, ScriptType.MANUAL.root() + "/"));
+        }
+        if (StringUtils.startsWith(id, ScriptRepository.ROOT + "/")) {
             return String.format("Script '%s'", StringUtils.removeStart(id, ScriptRepository.ROOT + "/"));
         }
         return String.format("Executable '%s'", StringUtils.removeStart(id, AcmConstants.SETTINGS_ROOT + "/"));
