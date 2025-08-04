9e773392b89eeefad6c4fc8485c472e9c3e7be18
Krystian Panek <krystian.panek@vml.com>
2025-08-04 08:39:03 +0200

    Safe script execution

--- core/src/main/java/dev/vml/es/acm/core/script/AutomaticScriptScheduler.java
+++ core/src/main/java/dev/vml/es/acm/core/script/AutomaticScriptScheduler.java
@@ -1,5 +1,8 @@
 package dev.vml.es.acm.core.script;
 
+import dev.vml.es.acm.core.code.ExecutionQueue;
+import dev.vml.es.acm.core.instance.HealthChecker;
+import dev.vml.es.acm.core.instance.HealthStatus;
 import dev.vml.es.acm.core.script.schedule.BootSchedule;
 import dev.vml.es.acm.core.script.schedule.CronSchedule;
 import dev.vml.es.acm.core.util.ResourceUtils;
@@ -9,6 +12,7 @@ import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.api.resource.observation.ResourceChange;
 import org.apache.sling.api.resource.observation.ResourceChangeListener;
+import org.apache.sling.commons.scheduler.Job;
 import org.apache.sling.commons.scheduler.ScheduleOptions;
 import org.apache.sling.commons.scheduler.Scheduler;
 import org.jetbrains.annotations.NotNull;
@@ -45,19 +49,19 @@ public class AutomaticScriptScheduler implements ResourceChangeListener {
     @Reference
     private Scheduler scheduler;
 
+    @Reference
+    private HealthChecker healthChecker;
+
+    @Reference
+    private ExecutionQueue executionQueue;
+
     @Activate
     @Modified
     protected void activate() {
         try (ResourceResolver resourceResolver = ResourceUtils.contentResolver(resourceResolverFactory, null)) {
             ScriptRepository scriptRepository = new ScriptRepository(resourceResolver);
             scriptRepository.findAll(ScriptType.SCHEDULE).forEach(script -> schedule(script.getPath(), resourceResolver));
-            ScheduleOptions options = configureSchedule("boot", scheduler.NOW());
-            scheduler.schedule(new Runnable() {
-                @Override
-                public void run() {
-                    // TODO ... loop over all schedules that schedule is 'boot'
-                }
-            }, options);
+            scheduler.schedule(bootJob(), configureSchedule("boot", scheduler.NOW()));
         } catch (LoginException e) {
             LOG.error("Cannot access repository while starting automatic script scheduler!", e);
         }
@@ -115,14 +119,7 @@ public class AutomaticScriptScheduler implements ResourceChangeListener {
         } else if (schedule instanceof CronSchedule) {
             CronSchedule cron = (CronSchedule) schedule;
             if (StringUtils.isNotBlank(cron.getExpression())) {
-                ScheduleOptions options = configureSchedule(scriptPath, scheduler.EXPR(cron.getExpression()));
-                configureSchedule(scriptPath, options);
-                scheduler.schedule(new Runnable() {
-                    @Override
-                    public void run() {
-                        // TODO add script to the queue
-                    }
-                }, options);
+                scheduler.schedule(cronJob(), configureSchedule(scriptPath, scheduler.EXPR(cron.getExpression())));
                 schedules.put(scriptPath, schedule);
             } else {
                 LOG.error("Schedule '{}' for script '{}' has no cron expression defined!", schedule.getId(), scriptPath);
@@ -134,5 +131,37 @@ public class AutomaticScriptScheduler implements ResourceChangeListener {
 
     private ScriptSchedule determineSchedule(String scriptPath, ResourceResolver resourceResolver) {
         return new BootSchedule(); // call script's scheduleRun() method
+    }
+
+    private Job bootJob() {
+        return context -> {
+            HealthStatus healthStatus = null;
+            while (healthStatus == null || !healthStatus.isHealthy()) {
+                healthStatus = healthChecker.checkStatus();
+                if (healthStatus.isHealthy()) {
+                    break;
+                } else {
+                    LOG.warn("Boot script scheduler is paused due to health status: {}", healthStatus);
+                    try {
+                        Thread.sleep(1000); // wait before retrying
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        LOG.error("Boot script scheduler interrupted!", e);
+                    }
+                }
+            }
+            LOG.info("Boot script scheduler is starting...");
+            for (Map.Entry<String, ScriptSchedule> entry : schedules.entrySet()) { // TODO do not boot twice same scripts
+                if (entry.getValue() instanceof BootSchedule) {
+                    // TODO executionQueue.submit()
+                }
+            }
+        };
+    }
+
+    private Job cronJob() {
+        return context -> {
+            // TODO ...
+        };
     }
 }
