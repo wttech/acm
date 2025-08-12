f421c1db219096f360e20ecbf7e38f38875da428
Krystian Panek <krystian.panek@vml.com>
2025-08-12 11:57:35 +0200

    Safe script execution

--- core/src/main/java/dev/vml/es/acm/core/AcmConstants.java
+++ core/src/main/java/dev/vml/es/acm/core/AcmConstants.java
@@ -2,9 +2,6 @@ package dev.vml.es.acm.core;
 
 public class AcmConstants {
 
-    // TODO move scripts and stuff here
-    public static final String APPS_ROOT = "/apps/acm";
-
     public static final String SETTINGS_ROOT = "/conf/acm/settings";
 
     public static final String VAR_ROOT = "/var/acm";
