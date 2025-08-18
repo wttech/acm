747e28e23ef12bf3e17cfdaf90fff4bc848024cd
Krystian Panek <krystian.panek@vml.com>
2025-08-18 09:53:26 +0200

    Events

--- core/src/main/java/dev/vml/es/acm/core/event/ResourceEvent.java
+++ core/src/main/java/dev/vml/es/acm/core/event/ResourceEvent.java
@@ -33,6 +33,7 @@ public class ResourceEvent implements Event {
         Map<String, Object> properties = new HashMap<>();
         properties.put(JcrConstants.JCR_PRIMARYTYPE, JcrConstants.NT_UNSTRUCTURED);
         properties.put(TRIGGERED_AT_PROP, Calendar.getInstance());
+        result.parent().ensureFolder();
         result.save(properties);
         return new ResourceEvent(result.require());
     }
