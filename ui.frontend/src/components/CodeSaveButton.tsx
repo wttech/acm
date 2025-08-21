065e115f1071fa1956e4fdf3daf79517a062441c
Krystian Panek <krystian.panek@vml.com>
2025-08-21 10:07:51 +0200

    Script sync all + delete

--- ui.frontend/src/components/CodeSaveButton.tsx
+++ ui.frontend/src/components/CodeSaveButton.tsx
@@ -56,7 +56,6 @@ const CodeSaveButton: React.FC<CodeSaveButtonProps> = ({ code, ...buttonProps })
     reset(getFormDefaults(code));
   };
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const onSubmit = async (data: CodeFormValues) => {
     setSaving(true);
     try {
@@ -71,8 +70,12 @@ const CodeSaveButton: React.FC<CodeSaveButtonProps> = ({ code, ...buttonProps })
           },
         },
       });
-      if (sync) {
-        // TODO sync if checked
+      if (data.sync) {
+        await toastRequest({
+          method: 'POST',
+          url: `/apps/acm/api/script.json?action=sync`,
+          operation: `Synchronize scripts`,
+        });
       }
       handleClose();
     } finally {
@@ -81,82 +84,82 @@ const CodeSaveButton: React.FC<CodeSaveButtonProps> = ({ code, ...buttonProps })
   };
 
   return (
-    <>
-      <Button onPress={handleOpen} {...buttonProps}>
-        <FileAdd />
-        <Text>Save</Text>
-      </Button>
-      <DialogContainer onDismiss={handleClose}>
-        {dialogOpen && (
-          <Dialog minWidth="40vw">
-            <FormProvider {...methods}>
-              <Heading>Save Script</Heading>
-              <Divider />
-              <Content>
-                <Form onSubmit={handleSubmit(onSubmit)}>
-                  <Controller
-                    name="scriptType"
-                    control={control}
-                    render={({ field }) => (
-                      <RadioGroup {...field} label="Type" isRequired value={field.value} onChange={field.onChange} orientation="horizontal">
-                        <Radio value={ScriptType.MANUAL}>
-                          <Hand size="XS" />
-                          <Text marginStart="size-50">Manual</Text>
-                        </Radio>
-                        <Radio value={ScriptType.AUTOMATIC}>
-                          <Launch size="XS" />
-                          <Text marginStart="size-50">Automatic</Text>
-                        </Radio>
-                      </RadioGroup>
+      <>
+        <Button onPress={handleOpen} {...buttonProps}>
+          <FileAdd />
+          <Text>Save</Text>
+        </Button>
+        <DialogContainer onDismiss={handleClose}>
+          {dialogOpen && (
+              <Dialog minWidth="40vw">
+                <FormProvider {...methods}>
+                  <Heading>Save Script</Heading>
+                  <Divider />
+                  <Content>
+                    <Form onSubmit={handleSubmit(onSubmit)}>
+                      <Controller
+                          name="scriptType"
+                          control={control}
+                          render={({ field }) => (
+                              <RadioGroup {...field} label="Type" isRequired value={field.value} onChange={field.onChange} orientation="horizontal">
+                                <Radio value={ScriptType.MANUAL}>
+                                  <Hand size="XS" />
+                                  <Text marginStart="size-50">Manual</Text>
+                                </Radio>
+                                <Radio value={ScriptType.AUTOMATIC}>
+                                  <Launch size="XS" />
+                                  <Text marginStart="size-50">Automatic</Text>
+                                </Radio>
+                              </RadioGroup>
+                          )}
+                      />
+                      <Controller
+                          name="scriptName"
+                          control={control}
+                          rules={{
+                            required: 'Value is required',
+                            validate: (value) => Strings.checkFilePath(value) || 'Value has invalid format',
+                          }}
+                          render={({ field }) => (
+                              <TextField {...field} label="Name" width="100%" isRequired marginTop="size-200" validationState={formState.errors.scriptName ? 'invalid' : undefined} errorMessage={formState.errors.scriptName?.message} />
+                          )}
+                      />
+                      <TextField label="ID" width="100%" value={scriptId} isDisabled marginTop="size-200" />
+                      <Controller
+                          name="sync"
+                          control={control}
+                          render={({ field: { value, onChange, onBlur, name, ref } }) => (
+                              <Checkbox isSelected={value} onChange={onChange} onBlur={onBlur} name={name} ref={ref} marginTop="size-200" isDisabled={scriptType === ScriptType.MANUAL}>
+                                <UploadToCloud size="XS" />
+                                <Text marginStart="size-50">Synchronize</Text>
+                              </Checkbox>
+                          )}
+                      />
+                    </Form>
+                    {scriptType === ScriptType.AUTOMATIC && (
+                        <InlineAlert width="100%" variant="notice" marginTop="size-100" UNSAFE_style={{ padding: '8px' }}>
+                          <Heading>Warning</Heading>
+                          <Content UNSAFE_style={{ padding: '6px', marginTop: '6px'}}>
+                            <Text>{sync ? 'This action may cause immediate execution on the author and publish instances.' : 'This action may cause immediate execution on the author instance.'}</Text>
+                          </Content>
+                        </InlineAlert>
                     )}
-                  />
-                  <Controller
-                    name="scriptName"
-                    control={control}
-                    rules={{
-                      required: 'Value is required',
-                      validate: (value) => Strings.checkFilePath(value) || 'Value has invalid format',
-                    }}
-                    render={({ field }) => (
-                      <TextField {...field} label="Name" width="100%" isRequired marginTop="size-200" validationState={formState.errors.scriptName ? 'invalid' : undefined} errorMessage={formState.errors.scriptName?.message} />
-                    )}
-                  />
-                  <TextField label="ID" width="100%" value={scriptId} isDisabled marginTop="size-200" />
-                  <Controller
-                    name="sync"
-                    control={control}
-                    render={({ field: { value, onChange, onBlur, name, ref } }) => (
-                      <Checkbox isSelected={value} onChange={onChange} onBlur={onBlur} name={name} ref={ref} marginTop="size-200">
-                        <UploadToCloud size="XS" />
-                        <Text marginStart="size-50">Synchronize</Text>
-                      </Checkbox>
-                    )}
-                  />
-                  {scriptType === ScriptType.AUTOMATIC && (
-                    <InlineAlert variant="notice" UNSAFE_style={{ padding: '12px' }}>
-                      <Heading>Warning</Heading>
-                      <Content UNSAFE_style={{ padding: '6px' }}>
-                        <Text>{sync ? 'This action may cause immediate execution on the author and publish instances.' : 'This action may cause immediate execution on the author instance.'}</Text>
-                      </Content>
-                    </InlineAlert>
-                  )}
-                </Form>
-              </Content>
-              <ButtonGroup>
-                <Button variant="secondary" onPress={handleClose}>
-                  <Close size="XS" />
-                  <Text>Cancel</Text>
-                </Button>
-                <Button variant="cta" isPending={saving} isDisabled={saving} onPress={() => handleSubmit(onSubmit)()} type="button">
-                  <Checkmark size="XS" />
-                  <Text>Save</Text>
-                </Button>
-              </ButtonGroup>
-            </FormProvider>
-          </Dialog>
-        )}
-      </DialogContainer>
-    </>
+                  </Content>
+                  <ButtonGroup>
+                    <Button variant="secondary" onPress={handleClose}>
+                      <Close size="XS" />
+                      <Text>Cancel</Text>
+                    </Button>
+                    <Button variant="cta" isPending={saving} isDisabled={saving} onPress={() => handleSubmit(onSubmit)()} type="button">
+                      <Checkmark size="XS" />
+                      <Text>Save</Text>
+                    </Button>
+                  </ButtonGroup>
+                </FormProvider>
+              </Dialog>
+          )}
+        </DialogContainer>
+      </>
   );
 };
 
