a57fb382d5637eb94994b788fee7510babfd5321
Krystian Panek <krystian.panek@vml.com>
2025-08-29 11:41:30 +0200

    Misc maintenance

--- ui.frontend/src/components/Misc.tsx
+++ ui.frontend/src/components/Misc.tsx
@@ -1,8 +1,15 @@
 import { Button, Cell, Column, Flex, Row, TableBody, TableHeader, TableView, Text } from '@adobe/react-spectrum';
-import Code from '@spectrum-icons/workflow/Code';
 import Settings from '@spectrum-icons/workflow/Settings';
+
+import Beaker from '@spectrum-icons/workflow/Beaker';
+import Filter from '@spectrum-icons/workflow/Filter';
+import History from '@spectrum-icons/workflow/History';
+import Info from '@spectrum-icons/workflow/Info';
+import WebPage from '@spectrum-icons/workflow/WebPage';
+import Data from '@spectrum-icons/workflow/Data';
+
 import { useAppState } from '../hooks/app';
-import { InstanceType } from '../utils/api.types';
+import {instancePrefix, InstanceType} from '../utils/api.types';
 import ExecutionHistoryClearButton from './ExecutionHistoryClearButton';
 
 const Misc = () => {
@@ -18,38 +25,25 @@ const Misc = () => {
         </TableHeader>
         <TableBody>
           <Row key="execution-history">
-            <Cell>Execution History</Cell>
+            <Cell>
+              <Flex gap="size-100">
+                <History size="S" />
+                <Text>Execution History</Text>
+              </Flex>
+            </Cell>
             <Cell>
               <ExecutionHistoryClearButton />
             </Cell>
           </Row>
           <Row key="spa-settings">
-            <Cell>Spa Settings</Cell>
-            <Cell>
-              <Button variant="secondary" isDisabled={isCloud} onPress={() => window.open('/system/console/configMgr/dev.vml.es.acm.core.gui.SpaSettings', '_blank', 'noopener')}>
-                <Settings />
-                <Text>Configure</Text>
-              </Button>
-            </Cell>
-          </Row>
-          <Row key="mock-http-filter">
-            <Cell>Mock HTTP Filter</Cell>
-            <Cell>
-              <Button variant="secondary" isDisabled={isCloud} onPress={() => window.open('/system/console/configMgr/com.vml.es.aem.acm.core.mock.MockHttpFilter', '_blank', 'noopener')}>
-                <Settings />
-                <Text>Configure</Text>
-              </Button>
-            </Cell>
-          </Row>
-          <Row key="instance-info">
             <Cell>
               <Flex gap="size-100">
-                <Code size="S" />
-                <Text>Instance Info</Text>
+                <WebPage size="S" />
+                <Text>Spa Settings</Text>
               </Flex>
             </Cell>
             <Cell>
-              <Button variant="secondary" isDisabled={isCloud} onPress={() => window.open('/system/console/configMgr/dev.vml.es.acm.core.osgi.InstanceInfo', '_blank', 'noopener')}>
+              <Button variant="secondary" isDisabled={isCloud} onPress={() => window.open(`${instancePrefix}/system/console/configMgr/dev.vml.es.acm.core.gui.SpaSettings`, '_blank', 'noopener')}>
                 <Settings />
                 <Text>Configure</Text>
               </Button>
@@ -58,12 +52,12 @@ const Misc = () => {
           <Row key="code-repository">
             <Cell>
               <Flex gap="size-100">
-                <Code size="S" />
+                <Data size="S" />
                 <Text>Code Repository</Text>
               </Flex>
             </Cell>
             <Cell>
-              <Button variant="secondary" isDisabled={isCloud} onPress={() => window.open('/system/console/configMgr/dev.vml.es.acm.core.code.CodeRepository', '_blank', 'noopener')}>
+              <Button variant="secondary" isDisabled={isCloud} onPress={() => window.open(`${instancePrefix}/system/console/configMgr/dev.vml.es.acm.core.code.CodeRepository`, '_blank', 'noopener')}>
                 <Settings />
                 <Text>Configure</Text>
               </Button>
@@ -72,12 +66,40 @@ const Misc = () => {
           <Row key="code-assistancer">
             <Cell>
               <Flex gap="size-100">
-                <Code size="S" />
+                <Beaker size="S" />
                 <Text>Code Assistancer</Text>
               </Flex>
             </Cell>
             <Cell>
-              <Button variant="secondary" isDisabled={isCloud} onPress={() => window.open('/system/console/configMgr/dev.vml.es.acm.core.assist.Assistancer', '_blank', 'noopener')}>
+              <Button variant="secondary" isDisabled={isCloud} onPress={() => window.open(`${instancePrefix}/system/console/configMgr/dev.vml.es.acm.core.assist.Assistancer`, '_blank', 'noopener')}>
+                <Settings />
+                <Text>Configure</Text>
+              </Button>
+            </Cell>
+          </Row>
+          <Row key="mock-http-filter">
+            <Cell>
+              <Flex gap="size-100">
+                <Filter size="S" />
+                <Text>Mock HTTP Filter</Text>
+              </Flex>
+            </Cell>
+            <Cell>
+              <Button variant="secondary" isDisabled={isCloud} onPress={() => window.open(`${instancePrefix}/system/console/configMgr/com.vml.es.aem.acm.core.mock.MockHttpFilter`, '_blank', 'noopener')}>
+                <Settings />
+                <Text>Configure</Text>
+              </Button>
+            </Cell>
+          </Row>
+          <Row key="instance-info">
+            <Cell>
+              <Flex gap="size-100">
+                <Info size="S" />
+                <Text>Instance Info</Text>
+              </Flex>
+            </Cell>
+            <Cell>
+              <Button variant="secondary" isDisabled={isCloud} onPress={() => window.open(`${instancePrefix}/system/console/configMgr/dev.vml.es.acm.core.osgi.InstanceInfo`, '_blank', 'noopener')}>
                 <Settings />
                 <Text>Configure</Text>
               </Button>
