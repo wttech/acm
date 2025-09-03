10d49c39c218d56f72e38e377d284b5d7f420497
Krystian Panek <krystian.panek@vml.com>
2025-09-03 09:59:01 +0200

    Misc GUI

--- ui.frontend/src/utils/api.types.ts
+++ ui.frontend/src/utils/api.types.ts
@@ -362,6 +362,9 @@ export enum InstanceOsgiServicePid {
   CODE_ASSISTANCER = 'dev.vml.es.acm.core.assist.Assistancer',
   MOCK_HTTP_FILTER = 'dev.vml.es.acm.core.mock.MockHttpFilter',
   INSTANCE_INFO = 'dev.vml.es.acm.core.osgi.InstanceInfo',
+
+  NOTIFIER_FACTORY_SLACK = "dev.vml.es.acm.core.notification.slack.SlackFactory",
+  NOTIFIER_FACTORY_TEAMS = "dev.vml.es.acm.core.notification.teams.TeamsFactory"
 }
 
 export function instanceOsgiServiceConfigUrl(pid: InstanceOsgiServicePid): string {
