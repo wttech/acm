83067209920aeaf2e41dd15c7b136a727894c08c
Krystian Panek <krystian.panek@vml.com>
2025-09-05 14:00:15 +0200

    Java 8 compat

--- core/src/main/java/dev/vml/es/acm/core/osgi/OsgiUtils.java
+++ core/src/main/java/dev/vml/es/acm/core/osgi/OsgiUtils.java
@@ -2,32 +2,28 @@ package dev.vml.es.acm.core.osgi;
 
 import org.osgi.framework.Bundle;
 
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * Utility methods for OSGi operations.
- */
 public final class OsgiUtils {
 
-    private static final Map<Integer, String> BUNDLE_STATE_NAMES;
-    
-    static {
-        Map<Integer, String> stateNames = new HashMap<>();
-        stateNames.put(Bundle.UNINSTALLED, "uninstalled");
-        stateNames.put(Bundle.INSTALLED, "installed");
-        stateNames.put(Bundle.RESOLVED, "resolved");
-        stateNames.put(Bundle.STARTING, "starting");
-        stateNames.put(Bundle.STOPPING, "stopping");
-        stateNames.put(Bundle.ACTIVE, "active");
-        BUNDLE_STATE_NAMES = stateNames;
-    }
-
     private OsgiUtils() {
-        // intentionally empty
+        // Utility class
     }
 
-    public static String bundleStateName(int state) {
-        return BUNDLE_STATE_NAMES.getOrDefault(state, String.format("state '%d'", state));
+    public static String getStateName(int state) {
+        switch (state) {
+            case Bundle.UNINSTALLED:
+                return "uninstalled";
+            case Bundle.INSTALLED:
+                return "installed";
+            case Bundle.RESOLVED:
+                return "resolved";
+            case Bundle.STARTING:
+                return "starting";
+            case Bundle.STOPPING:
+                return "stopping";
+            case Bundle.ACTIVE:
+                return "active";
+            default:
+                return String.format("state '%d'", state);
+        }
     }
 }
