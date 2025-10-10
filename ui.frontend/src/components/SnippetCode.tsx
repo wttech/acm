e477b1f4a74dcf034f98fab6918936f14ccfd358
Krystian Panek <krystian.panek@vml.com>
2025-10-10 16:54:18 +0200

    Minors

--- ui.frontend/src/components/SnippetCode.tsx
+++ ui.frontend/src/components/SnippetCode.tsx
@@ -1,8 +1,7 @@
 import hljs from 'highlight.js';
 import groovy from 'highlight.js/lib/languages/groovy';
-
 import 'highlight.js/styles/vs2015.min.css';
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './SnippetCode.css';
 
 hljs.registerLanguage('snippet-code-groovy', function (hljs) {
@@ -44,24 +43,26 @@ interface SnippetCodeProps {
 }
 
 const SnippetCode: React.FC<SnippetCodeProps> = ({ content }) => {
-  const codeRef = useRef<HTMLElement>(null);
+  const codeRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (codeRef.current) {
-      const element = codeRef.current;
-      // Clear previous highlighting by resetting content and removing data attribute
-      element.textContent = content;
-      delete element.dataset.highlighted;
-      element.className = 'snippet-code-groovy';
-      // Then highlight
-      hljs.highlightElement(element);
+      const codeElement = codeRef.current.querySelector('code');
+      if (codeElement) {
+        // Clear previous highlighting by resetting content and removing data attribute
+        codeElement.textContent = content;
+        delete codeElement.dataset.highlighted;
+        codeElement.className = 'snippet-code-groovy';
+        // Then highlight
+        hljs.highlightElement(codeElement);
+      }
     }
   }, [content]);
 
   return (
-    <pre>
-      <code ref={codeRef} className="snippet-code-groovy" />
-    </pre>
+    <div ref={codeRef}>
+      <code className="snippet-code-groovy">{content}</code>
+    </div>
   );
 };
 
