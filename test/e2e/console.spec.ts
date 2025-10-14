8c5baf98fd68e77285e62f4c3406f7ca34dc7606
Krystian Panek <krystian.panek@vml.com>
2025-10-14 10:10:20 +0200

    Console test

--- test/e2e/console.spec.ts
+++ test/e2e/console.spec.ts
@@ -5,4 +5,7 @@ test('Console executes script', async ({ page }) => {
 
     await page.getByRole('button', { name: 'Console' }).click();
     await page.getByRole('button', { name: 'Execute' }).click();
+
+    await page.getByText('Output').click();
+    await page.getByText('Hello World!').waitFor({ timeout: 15000 });
 });
