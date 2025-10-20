db08fc5d9c82b041aeb80acf0cf9af565ccde6ac
Krystian Panek <krystian.panek@vml.com>
2025-10-20 09:09:53 +0200

    Serial E2E

--- test/e2e/playwright.config.ts
+++ test/e2e/playwright.config.ts
@@ -14,7 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './.',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -37,7 +37,7 @@ export default defineConfig({
     trace: 'on-first-retry',
 
     /* Screenshots on failure */
-    screenshot: 'only-on-failure',
+    screenshot: 'on',
 
     /* Videos on failure */
     video: 'retain-on-failure',
