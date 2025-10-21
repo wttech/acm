533fa834787d64a52ef253d044356bad8dd63cc6
Krystian Panek <krystian.panek@vml.com>
2025-10-21 07:38:30 +0200

    Revert

--- ui.content.example/src/main/content/jcr_root/conf/acm/settings/script/extension/example/ACME-300_extension.groovy
+++ ui.content.example/src/main/content/jcr_root/conf/acm/settings/script/extension/example/ACME-300_extension.groovy
@@ -1,14 +1,19 @@
 import dev.vml.es.acm.core.code.ExecutionContext
 import dev.vml.es.acm.core.code.Execution
-import java.util.Calendar
 
 void prepareRun(ExecutionContext context) {
-    def minute = Calendar.getInstance().get(Calendar.MINUTE)
-    if (minute % 2 == 0) {
-        context.skipped = true
-    }
+    context.variable("acme", new AcmeFacade())
 }
 
 void completeRun(Execution execution) {
-    log.info "I am not skipped on even minutes!"
+    if (execution.status.name() == 'FAILED') {
+        log.error "Something nasty happened with '${execution.executable.id}'!"
+        // TODO send notification using built-in 'notifier' for Slack and MS Teams or use custom HTTP client / WebAPI
+    }
+}
+
+class AcmeFacade {
+    def now() {
+        return new Date()
+    }
 }
