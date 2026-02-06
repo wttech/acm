fca6f1d3e30f49f330ae9f0c3c8e6a271c8fdb31
Krystian Panek <krystian.panek@vml.com>
2026-02-06 09:35:21 +0100

    Groovy highlight regex fix + types

--- ui.frontend/src/utils/monaco/theme.ts
+++ ui.frontend/src/utils/monaco/theme.ts
@@ -1,1 +1,12 @@
-export const DEFAULT_THEME_ID = 'vs-dark';
+import { Monaco } from '@monaco-editor/react';
+
+export const DEFAULT_THEME_ID = 'acm-dark';
+
+export function registerTheme(instance: Monaco) {
+  instance.editor.defineTheme(DEFAULT_THEME_ID, {
+    base: 'vs-dark',
+    inherit: true,
+    rules: [],
+    colors: {},
+  });
+}
